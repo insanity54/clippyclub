1a75d54b31597d2c148a022a40fcc83a2ab94b05
chris <chris@grimtech.net>
2024-01-27 21:01:30 +0000

    add outros query

---
+++ pages/Compilation/jsobjects/helpers/helpers.js
@@ -0,0 +1,13 @@
+export default {
+	myVar1: [],
+	myVar2: {},
+	myFun1 () {
+		//	write code here
+		//	this.myVar1 = [1,2,3]
+	},
+	async outrosResults () {
+		return await Outros.data.records
+		//	use async-await or promises
+		//	await storeValue('varName', 'hello world')
+	}
+}
